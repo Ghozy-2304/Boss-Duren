9593bbeb5e962d2625a0b79a79cd06ae6c545e05
Ghozy <ghozy0432@gmail.com>
2025-09-15 14:16:49 +0700

    pembaruan navbar dan hero banner

---
+++ main.js
@@ -0,0 +1,42 @@
+const hamburger = document.getElementById('hamburger');
+    const mobileMenu = document.getElementById('mobileMenu');
+    const closeMenu = document.getElementById('closeMenu');
+    const mencetMenu = document.querySelectorAll('.menuMobile');
+
+    // buka menu
+    hamburger.addEventListener('click', () => {
+      mobileMenu.classList.remove('translate-x-full');
+      mobileMenu.classList.add('translate-x-0');
+    });
+
+    // tutup menu
+    closeMenu.addEventListener('click', () => {
+      mobileMenu.classList.remove('translate-x-0');
+      mobileMenu.classList.add('translate-x-full');
+    });
+
+    mencetMenu.forEach(menu => {
+      menu.addEventListener('click', () => {
+        mobileMenu.classList.remove('translate-x-0');
+        mobileMenu.classList.add('translate-x-full');
+      })
+    });
+
+// untuk membuat navbar text jadi putih saat di scroll
+
+const navbar = document.getElementById('navbar');
+const links = navbar.querySelectorAll('a');
+
+window.addEventListener('scroll', () => {
+  if(window.scrollY > 50) { // scroll > 50px
+    links.forEach(link => {
+      link.classList.remove('text-black');
+      link.classList.add('text-white');
+    });
+  } else {
+    links.forEach(link => {
+      link.classList.remove('text-white');
+      link.classList.add('text-black');
+    });
+  }
+});
